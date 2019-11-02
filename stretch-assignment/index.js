98495f000cfa2f0abf355f44c0a6a13b67aebfe8
Janessa Garrow <janessa.garrow@gmail.com>
2019-11-02 16:43:25 -0600

    Played around with different twinkling star backgrounds for stretch project. Found 3 different options, 2 of which are pretty laggy. I left the code in for reference, but commented the laggy options out

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,19 +3,87 @@ window.addEventListener('load', event => {
     const ships_array = [...document.querySelectorAll('.block')];
     const position_from_top_array = [0, 120, 240, 360, 480];
     const engineOn = "radial-gradient(ellipse 9px 18px at 38% 82%, orange 0%, orange 99%, transparent 100%), radial-gradient(ellipse 9px 18px at 60% 82%, orange 0%, orange 99%, transparent 100%)";
-    const stars = "rgba(255,255,255,0.0)";
+    // const stars = "rgba(255,255,255,0.0)";
     const travelLimit = 400; // limit in pixels
 
-    // for twinkling stars -- makes it kinda laggy, though
+    // Option 1 for twinkling stars -- makes it really laggy
     // used for reference: https://codepen.io/KyleShaver/pen/jARzzZ?editors=0110
-    for (let i = 0; i < 100; i++) {
-        let star = document.createElement('div');
-        star.classList.add('star');
-        star.style = 'animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*window.innerHeight+'px; left: '+Math.random()*window.innerWidth+'px;';
-        document.querySelector('.atmosphereContainer').append(star);
-      }
+    // for (let i = 0; i < 200; i++) {
+    //     let star = document.createElement('div');
+    //     star.classList.add('star');
+    //     star.style = 'animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*window.innerHeight+'px; left: '+Math.random()*window.innerWidth+'px;';
+    //     document.querySelector('.atmosphereContainer').append(star);
+    // }
+
+    // Option 2 for twinkling stars using HTML canvas -- also makes it a bit laggy (but not as laggy as option 1), but let's you have more control over the background colors 
+    // https://codepen.io/Jenbo/pen/pgmZwB?editors=1010
+    // function Star(x,y,r,color){
+    //     this.x = x;
+    //     this.y = y;
+    //     this.r = r;
+    //     this.rChange = 0.015;
+    //     this.color = color;
+    // }
+    
+    // Star.prototype = {
+    //     constructor: Star,
+    //     render: function(){
+    //       context.beginPath();
+    //       context.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
+    //       context.shadowBlur = 8; 
+    //       context.shadowColor = "white";
+    //       context.fillStyle = this.color;
+    //       context.fill();
+    //     },
+    //     update: function(){
+          
+    //        if (this.r > 2 || this.r < .8){
+    //            this.rChange = - this.rChange;
+    //        }
+    //        this.r += this.rChange;
+    //     }
+    // }
+    
+    // var canvas = document.getElementById("canvas");
+    // var context = canvas.getContext("2d");
+    
+    // var C_WIDTH = canvas.width = document.body.offsetWidth;
+    // var C_HEIGHT = canvas.height = document.body.offsetHeight;
+    
+    // function randomColor(){
+    //         var arrColors = ["ffffff", "ffecd3" , "bfcfff"];
+    //         return "#"+arrColors[Math.floor((Math.random()*3))];
+    // }
+            
+    // var arrStars = [];
+    // for(i = 0; i < 400; i++){
+    //     var randX = Math.floor((Math.random()*C_WIDTH)+1);
+    //     var randY = Math.floor((Math.random()*C_HEIGHT)+1);
+    //     var randR = Math.random() * 1.7 + .5;
+        
+    //     var star = new Star(randX, randY, randR, randomColor());
+    //     arrStars.push(star);
+    // }
+    // function update(){
+    //   for(i = 0; i < arrStars.length; i ++){
+    //     arrStars[i].update();
+    //   }
+    // }
+    // function animate(){
+    //     update();
+    //     context.clearRect(0,0,C_WIDTH,C_HEIGHT);
+    //     for(var i = 0; i < arrStars.length; i++){
+    //       arrStars[i].render();
+    //     }
+    //     requestAnimationFrame(animate);
+    // }
+    // animate();
+
+    // Option 3 for twinkling stars -- no JavaScript, best performance, but background color has to be all black
+    // http://www.script-tutorials.com/night-sky-with-twinkling-stars/
 
 
+    // Rocket Ship movement
     blocks.style.position = 'relative'; // lets the blocks inside of it have absolute positioning relative to this div
 
     // initialize ship positions
